df25793abfd4ad148b55b026165b261d1dfcba59
duckey <tomekch6@gmail.com>
2017-10-04 20:18:26 +0200

    add Articles tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -20,3 +20,18 @@ test('RecentlyChangedArticles must return an object', () => {
     expect(typeof data).toBe('object')
   })
 })
+
+test('ArticleAsSimpleJson must return an object', () => {
+  expect.assertions(1)
+  return new WikiaAPI('dev').getArticleAsSimpleJson(12649).then(data => {
+    console.log(data)
+    expect(typeof data).toBe('object')
+  })
+})
+
+test('ArticleAsSimpleJson should catch on 404', () => {
+  expect.assertions(1)
+  return new WikiaAPI('dev').getArticleAsSimpleJson(1).catch(error => {
+    expect(error.statusCode).toBe(404)
+  })
+})

eb218e4390d272a17497c31ae56da0c7b565a9f9
duckey <tomekch6@gmail.com>
2017-10-04 19:54:29 +0200

    add getRecentlyChangedArticles

--- main.js
+++ main.js
@@ -62,6 +62,48 @@ class WikiaAPI {
   }
 
   /**
+   * Get recently changed articles
+   * @see [Wikia API Documentation]{@link http://dev.wikia.com/api/v1#!/Activity/getRecentlyChangedArticles_get_1}
+   *
+   * @param {number} limit - Limit the number of results
+   * @param {(number[]|number)} namespaces - Array of namespace ids or a single namespace id, see more: {@link http://community.wikia.com/wiki/Help:Namespaces}
+   * @param {boolean} [allowDuplicates=true] - Set if duplicate values of an article's revisions made by the same user are not allowed
+   * @return {Promise<Object, Error>} A Promise with an Object containing recently changed articles on fulfil, and Error on rejection
+   *
+   * @instance
+   * @memberof WikiaAPI
+   */
+  getRecentlyChangedArticles (limit, namespaces, allowDuplicates = true) {
+    if (isNaN(limit)) {
+      throw new Error('Argument \'limit\' is required')
+    }
+    if (namespaces === undefined) {
+      throw new Error('Argument \'namespaces\' is required')
+    }
+
+    let ns = []
+    if (Array.isArray(namespaces)) {
+      ns = namespaces
+    } else if (!isNaN(namespaces)) {
+      ns = [namespaces]
+    } else {
+      throw new Error('Argument \'namespaces\' must be a number or array of numbers')
+    }
+
+    return new Promise((resolve, reject) => {
+      this._makeRequest('Activity/RecentlyChangedArticles', {
+        limit: limit,
+        namespaces: ns.join(','),
+        allowDuplicates: allowDuplicates
+      }).then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+
+  /**
    * Basepath of Wikia API V1 for given subdomain
    * @name WikiaAPI#url
    * @type {string}
@@ -80,7 +122,7 @@ class WikiaAPI {
       for (let param in params) {
         query.push(param + '=' + encodeURIComponent(params[param]))
       }
-      console.log(`${this.url}${endpoint}?${query.join('&')}`)
+
       got(`${this.url}${endpoint}?${query.join('&')}`).then(response => {
         let body
         try {

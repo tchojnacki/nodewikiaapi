ef0e0a3df6ae0bd692f2f5869a19b7db2895b92c
duckey <tomekch6@gmail.com>
2017-10-04 16:09:12 +0200

    Add query params to _makeRequest

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ class WikiaAPI {
    * @see [Wikia API Documentation]{@link http://dev.wikia.com/api/v1#!/Activity/getLatestActivity_get_0}
    *
    * @param {number} limit - Limit the number of results
-   * @param {(number[]|number)} namespaces - Array of namespace ids, see more: {@link http://community.wikia.com/wiki/Help:Namespaces}
+   * @param {(number[]|number)} namespaces - Array of namespace ids or a single namespace id, see more: {@link http://community.wikia.com/wiki/Help:Namespaces}
    * @param {boolean} [allowDuplicates=true] - Set if duplicate values of an article's revisions made by the same user are not allowed
    * @return {Promise<Object, Error>} A Promise with an Object containing latest activity on fulfil, and Error on rejection
    *
@@ -35,12 +35,25 @@ class WikiaAPI {
     if (isNaN(limit)) {
       throw new Error('Argument \'limit\' is required')
     }
-    if (!namespaces) {
+    if (namespaces === undefined) {
       throw new Error('Argument \'namespaces\' is required')
     }
 
+    let ns = []
+    if (Array.isArray(namespaces)) {
+      ns = namespaces
+    } else if (!isNaN(namespaces)) {
+      ns = [namespaces]
+    } else {
+      throw new Error('Argument \'namespaces\' must be a number or array of numbers')
+    }
+
     return new Promise((resolve, reject) => {
-      this._makeRequest('Activity/LatestActivity').then(response => {
+      this._makeRequest('Activity/LatestActivity', {
+        limit: limit,
+        namespaces: ns.join(','),
+        allowDuplicates: allowDuplicates
+      }).then(response => {
         resolve(response)
       }).catch(error => {
         reject(error)
@@ -49,7 +62,7 @@ class WikiaAPI {
   }
 
   /**
-   * Root of Wikia API V1 for given subdomain
+   * Basepath of Wikia API V1 for given subdomain
    * @name WikiaAPI#url
    * @type {string}
    * @readonly
@@ -57,17 +70,27 @@ class WikiaAPI {
   get url () {
     return `http://${this.subdomain}.wikia.com/api/v1/`
   }
-
   set url (value) {
     throw new Error('Cannot set a read-only property \'url\'')
   }
 
   _makeRequest (endpoint, params) {
     return new Promise((resolve, reject) => {
-      got(this.url + endpoint).then(response => {
-        resolve(response.body)
+      let query = []
+      for (let param in params) {
+        query.push(param + '=' + encodeURIComponent(params[param]))
+      }
+      console.log(`${this.url}${endpoint}?${query.join('&')}`)
+      got(`${this.url}${endpoint}?${query.join('&')}`).then(response => {
+        let body
+        try {
+          body = JSON.parse(response.body)
+        } catch (error) {
+          reject(new Error('Community not found'))
+        }
+        resolve(body)
       }).catch(error => {
-        reject(error.response.body)
+        reject(error)
       })
     })
   }

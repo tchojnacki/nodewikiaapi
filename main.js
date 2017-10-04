3a1fdbb037fa1e54e1e0dab4cb30b0cf73031839
duckey <tomekch6@gmail.com>
2017-10-04 15:28:40 +0200

    Create and document first function

--- main.js
+++ main.js
@@ -1,15 +1,75 @@
 'use strict'
 
+const got = require('got')
+
 /**
- * Main class
- * @param {string} url - Wiki url, for example "dev" or "pl.community"
+ * Class representing WikiaAPI V1
+ * @param {string} subdomain - Subdomain, for example "dev" or "pl.community"
  */
 class WikiaAPI {
-  constructor (url) {
-    if (!url) {
-      throw new Error('Argument \'url\' is required')
+  constructor (subdomain) {
+    if (!subdomain) {
+      throw new Error('Argument \'subdomain\' is required')
     }
-    this.url = url
+    /**
+     * Subdomain, for example "dev" or "pl.community"
+     * @name WikiaAPI#subdomain
+     * @type {string}
+     */
+    this.subdomain = subdomain
+  }
+
+  /**
+   * Get latest activity information
+   * @see [Wikia API Documentation]{@link http://dev.wikia.com/api/v1#!/Activity/getLatestActivity_get_0}
+   *
+   * @param {number} limit - Limit the number of results
+   * @param {(number[]|number)} namespaces - Array of namespace ids, see more: {@link http://community.wikia.com/wiki/Help:Namespaces}
+   * @param {boolean} [allowDuplicates=true] - Set if duplicate values of an article's revisions made by the same user are not allowed
+   * @return {Promise<Object, Error>} A Promise with an Object containing latest activity on fulfil, and Error on rejection
+   *
+   * @instance
+   * @memberof WikiaAPI
+   */
+  getLatestActivity (limit, namespaces, allowDuplicates = true) {
+    if (isNaN(limit)) {
+      throw new Error('Argument \'limit\' is required')
+    }
+    if (!namespaces) {
+      throw new Error('Argument \'namespaces\' is required')
+    }
+
+    return new Promise((resolve, reject) => {
+      this._makeRequest('Activity/LatestActivity').then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+
+  /**
+   * Root of Wikia API V1 for given subdomain
+   * @name WikiaAPI#url
+   * @type {string}
+   * @readonly
+   */
+  get url () {
+    return `http://${this.subdomain}.wikia.com/api/v1/`
+  }
+
+  set url (value) {
+    throw new Error('Cannot set a read-only property \'url\'')
+  }
+
+  _makeRequest (endpoint, params) {
+    return new Promise((resolve, reject) => {
+      got(this.url + endpoint).then(response => {
+        resolve(response.body)
+      }).catch(error => {
+        reject(error.response.body)
+      })
+    })
   }
 }
 

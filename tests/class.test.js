f933614cdee52ef1e74cb25c06075f9033dea0b5
duckey <tomekch6@gmail.com>
2017-10-04 16:35:33 +0200

    Add more tests

--- tests/class.test.js
+++ tests/class.test.js
@@ -1,5 +1,17 @@
 const WikiaAPI = require('../main')
 
+test('must have a subdomain', () => {
+  expect(() => { new WikiaAPI() }).toThrow()
+  expect(() => { new WikiaAPI('') }).toThrow()
+})
+
 test('returns correct subdomain', () => {
   expect(new WikiaAPI('dev').subdomain).toBe('dev')
+})
+
+test('can\'t set url', () => {
+  expect(() => {
+    const wikia = new WikiaAPI('dev')
+    wikia.url = 'community'
+  }).toThrow()
 })
